def10f5b18156261f80e1edb734955b3324e87fd
Simonin Lucas <lucas.simonin@cgi.com>
2016-11-30 13:30:16 +0100

    remove wow effect

--- src/App/Bundle/SiteBundle/Resources/public/js/script.js
+++ src/App/Bundle/SiteBundle/Resources/public/js/script.js
@@ -34,63 +34,9 @@
   }, 300);
 
 })(jQuery); // End of use strict
-var paddingPicture = function() {
-    var big = $('.bigpicture').height();
-    var bottom = $('.bottompicture').height();
-    var top = $('.toppicture').height();
-    paddingTop = big - bottom - top -1;
-    console.log(paddingTop, big, bottom, top);
-    if(paddingTop > 0) {
-        $('.bottompicture').css('padding-top', paddingTop);
-    }
-}
 
 $( document ).ready(function() {
-
-  var initialPosition = $('#mainNav').offset().top,
-    positionTop = false;
-
-  new WOW().init();
-
-  $(document).on('click', '.img-wrap',function() {
-    $('#mainNav').addClass('hide');
-  })
-
-  /*$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if(!positionTop) {
-      initialPosition = $('#mainNav').offset().top;
-    }
-    if(height  > $('#mainNav').offset().top && !positionTop) {
-      positionTop = true;
-      $('#mainNav').addClass('navbar-fixed-top');
-    }
-    if(height  <= initialPosition) {
-      $('#mainNav').removeClass('navbar-fixed-top');
-      positionTop = false;
-    }
-  });*/
-
-
     $('.multi-item-carousel').carousel({
         interval: false
     });
-
-
-       /* $('.multi-item-carousel .item').each(function(){
-            var next = $(this).next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
-            next.children(':first-child').clone().appendTo($(this));
-
-            if (next.next().length>0) {
-                next.next().children(':first-child').clone().appendTo($(this));
-            } else {
-                $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-            }
-        });
-    if($('.multi-item-carousel .item').length == 2) {
-        $('.multi-item-carousel .item.active .item-picture:last-child').html('')
-    }*/
 });
